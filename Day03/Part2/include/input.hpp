89992bdd015bb3019db4502e989ad84787f41c7d
Maniko <louis.chabrerie@innowide.fr>
2023-12-03 13:12:50 +0100

    Add part 2 test

---
+++ Day03/Part2/include/input.hpp
@@ -0,0 +1,13 @@
+#ifndef INPUT_HPP
+    #define INPUT_HPP
+
+    #include <Arduino.h>
+
+    #define INPUT_SIZE_X 140
+    #define INPUT_SIZE_Y 140
+
+    #define INPUT_TEST_SIZE_X 10
+    #define INPUT_TEST_SIZE_Y 10
+
+
+#endif /* INPUT_HPP */
